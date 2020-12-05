8588a7d7f0e69a81ba063f90493bc7edccfb03bb
Imad Rafai <imad.rafai@grenoble-inp.org>
2020-12-05 20:42:14 +0100

    Creation/Suppression  des types de flux

--- FluxText.java
+++ FluxText.java
@@ -3,35 +3,30 @@
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class FLuxVideo {
+public class FluxText {
 	
-	// Je suppose qu'il y aura une classe "Flux" mère des classes "FluxVideo", "FluxAudio", ... , et c'est elle qui s'occupera des données communes aux flux.
 	
 	//----------------------------------------------------------------------------------------------------------------//
 	//																												  //
 	//																												  //
-	//					Fonction créant un flux vidéo en lui passant les paramètres adéquats.						  //
+	//					Fonction créant un flux text en lui passant les paramètres adéquats.						  //
 	//																												  //
 	//																												  //
 	//																												  //
 	//----------------------------------------------------------------------------------------------------------------//
 	
-	public static void addFluxVideo(int idFichier, String langue, String codec, int debit, int resLargeurVid, int resHauteurVid) throws SQLException {
+	public static void addFluxText(int idFichier, String langue, String codec, int debit) throws SQLException {
 		
-		// on pourra remonter la recherche dans la classe mère
 		
-		PreparedStatement statement = BdClass.getConnection().prepareStatement("INSERT INTO Flux (Debit, Type, ResLargeurVid, ResLongeurVid, Codec, Langue, IdFichier) values(?,\'video\',?,?,?,?,?)");
+		PreparedStatement statement = BdClass.getConnection().prepareStatement("INSERT INTO Flux (Debit, Type, Codec, Langue, IdFichier) values(?,\'text\',?,?,?)");
 
 		statement.setInt(1, debit);
-		statement.setInt(2, resLargeurVid);
-		statement.setInt(3, resHauteurVid);
-		statement.setString(4, codec);
-		statement.setString(5, langue);
-		statement.setInt(6, idFichier);
+		statement.setString(2, codec);
+		statement.setString(3, langue);
+		statement.setInt(4, idFichier);
 
 		statement.executeQuery();
 		
-
 		BdClass.getConnection().commit();
 
 	}
@@ -39,16 +34,17 @@ public class FLuxVideo {
 		//----------------------------------------------------------------------------------------------------------------//
 		//																												  //
 		//																												  //
-		//					Fonction supprimant un flux video en lui passant les paramètres adéquats.					  //
+		//					Fonction supprimant un flux text en lui passant les paramètres adéquats.					  //
 		//																												  //
 		//																												  //
 		//																												  //
 		//----------------------------------------------------------------------------------------------------------------//
 	
-	public static void deleteFluxVideo(int noFLux) throws SQLException {
+	public static void deleteFluxText(int noFLux) throws SQLException {
 			
+			// on pourra remonter la recherche dans la classe mère
 			
-			PreparedStatement statement = BdClass.getConnection().prepareStatement("DELETE FROM Flux where noFlux = ? and type = \'video\' ");
+			PreparedStatement statement = BdClass.getConnection().prepareStatement("DELETE FROM Flux where noFlux = ? and type = \'text\' ");
 	
 			
 			statement.setInt(1, noFLux);
